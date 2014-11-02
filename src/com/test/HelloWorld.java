165253e03027652cb4b0b8584da59370529f33a6
maijianwei <jianwei_mai@163.com>
2014-11-02 20:29:37 +0800

    第一个提交更新

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		System.out.println("==================");
+		System.out.println("========3434==========");
 	}
 
 }
